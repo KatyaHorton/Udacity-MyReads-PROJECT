6dfad2ba6fbef628ded816b0a4e81a67e61e66da
Katya Horton <ekaterina.horton@gmail.com>
2018-08-06 14:23:30 +0100

    books fetch works, trying to adjust the code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+
+import * as BooksAPI from './BooksAPI'
 import './App.css'
+
 
 //import BooksSearch and BooksShelf componenets
 import BooksSearch from './BooksSearch'
@@ -11,28 +13,49 @@ import { Route } from 'react-router-dom'
 
 
 class BooksApp extends React.Component {
-  state = {
- 
+	
+	
+//creates an empty books array 
+  state = { 
+ 	books: []
   }
+
+//this function is going to invoked by react whenever that component is mounted to the view 	
+componentDidMount(){
+//calling BooksAPI.getAll() will return us a promise
+		BooksAPI.getAll().then((books) => {
+		this.setState({ books: books})
+	})}	
 
   render() {
     return (
 
       <div className="app">
 
-{/*Creates exact path to BooksShelf UI */}
+		{console.log(this.state.books)}
+	
+	  
+{/* creates exact path to BooksShelf UI */}
         <Route exact path='/'
 			render={() =>
-			(<BooksShelf />	
+			(<BooksShelf 
+// adds props to BooksShelf so we will be able to access books 
+			  books={this.state.books}
+			 />	
 		)}/>
 
 {/*Creates a path to BooksSearch UI */}			 
 		<Route path='/search'
 			render={() =>
-			(<BooksSearch />	
+			(<BooksSearch 
+// adds props to BooksSearch so we will be able to access books 
+			  books={this.state.books}					
+					/>	
 		)}/>
 			 
+ 
       </div>
+
     )
   }
 }
