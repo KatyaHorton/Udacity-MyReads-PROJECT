22f83774718d977d4de53d78ae9a13f4252bd332
Katya Horton <ekaterina.horton@gmail.com>
2018-08-06 15:15:23 +0100

    input in BooksSearch connected with query

--- src/BooksSearch.js
+++ src/BooksSearch.js
@@ -5,6 +5,9 @@ import './App.css'
 //imports Link component
 import { Link } from 'react-router-dom' 
 
+//imports packages to update state to filter books
+import escapeRegExp from 'escape-string-regexp' 
+import sortBy from 'sort-by'
 
 class BooksSearch extends React.Component {
  
@@ -19,12 +22,26 @@ updateQuery = (query) => {
 }
 
   render() {
+	  
+  let showBooks
+  	if (this.state.query) {
+		const match = new RegExp(escapeRegExp(this.state.query), 'i')
+		showBooks = this.props.books.filter(
+		(book) => match.test(book.title)
+         									)
+		} else {
+			showBooks = this.props.books
+		}
+	  
+//sorts books by name		
+		showBooks.sort(sortBy('name'))
+	  
     return (
       <div>
 		          <div className="search-books">
             <div className="search-books-bar">
            
-	
+
 		<Link to='/' 
 				className="close-search">
 					Close
@@ -49,11 +66,32 @@ updateQuery = (query) => {
 					onChange={(event) => this.updateQuery(event.target.value)}
 			
 		/>
-{JSON.stringify(this.state.query)}
               </div>
             </div>
             <div className="search-books-results">
               <ol className="books-grid">
+				 {showBooks.map((book) => (
+					<li>
+					<div className="book">
+							<div className="book-top">
+								<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url()' }}></div>
+							<div className="book-shelf-changer"> 	    
+								<select>
+									<option value="move" disabled>Move to...</option>
+									<option value="currentlyReading">Currently Reading</option>
+									<option value="wantToRead">Want to Read</option>
+									<option value="read">Read</option>
+									<option value="none">None</option>
+								</select>
+							</div>        
+						</div>
+						 <div className="book-title">{book.title}</div>
+						 <div className="book-authors">{book.authors}</div>
+					</div>
+					</li>
+	))}
+				  
+				  
 			  </ol>
             </div>
           </div>
@@ -62,5 +100,6 @@ updateQuery = (query) => {
     )
   }
 }
+
 
 export default BooksSearch
