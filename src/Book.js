e335d8acdab6a02e5c036d1cba21ad3bee213810
Katya Horton <ekaterina.horton@gmail.com>
2018-08-07 12:09:29 +0100

    select button added back to the Book component

--- src/Book.js
+++ src/Book.js
@@ -1,25 +1,26 @@
 import React from 'react'
 
-//import './App.css'
-
-//imports Link component
-//import { Link } from 'react-router-dom' 
-
-//imports packages to update state to filter books
-//import escapeRegExp from 'escape-string-regexp' 
-//import sortBy from 'sort-by'
-
 
 class Book extends React.Component {
  
 	
   render() {	  
     return (
-	
 		<div className='book' >
 			<div className='book-top'>
 				<div className='book-cover' style={{ width: 128, height: 193, backgroundImage: 'url()' }}></div>
-				<div className='book-shelf-changer'></div>
+					<div className='book-shelf-changer'>
+		<select>
+                    <option value="move" disabled>Move to...</option>
+                    <option value="currentlyReading">Currently Reading</option>
+                    <option value="wantToRead">Want to Read</option>
+                    <option value="read">Read</option>
+                    <option value="none">None</option>
+                </select>
+		
+		
+	
+				</div>
 				</div>
 				 <div className='book-title'>{this.props.book.title}</div>
 				 <div className='book-authors'>{this.props.book.authors}</div>
