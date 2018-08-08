72e1ac3fdb17824073a2269f58bee2bd2719e34d
Katya Horton <ekaterina.horton@gmail.com>
2018-08-08 21:47:40 +0100

    image src fixed

--- src/Book.js
+++ src/Book.js
@@ -17,7 +17,9 @@ changeShelf = (event) => {
     return (
 		<div className='book' >
 			<div className='book-top'>
-				<div className='book-cover' style={{ width: 128, height: 193, backgroundImage: `url("${this.props.book.imageLinks.thumbnail}")` }}></div>
+				<div className="book-cover">
+					<img src={this.props.book.imageLinks ? this.props.book.imageLinks.thumbnail : "No image found"} alt={`book cover of ${this.props.book.title}`}/>
+				</div>
 					<div className='book-shelf-changer'>
 						<select
 							value={this.state.currentShelf}
