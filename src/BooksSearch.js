b44becbacb7e62a14332499799a81981fd3a9a25
Katya Horton <ekaterina.horton@gmail.com>
2018-08-09 14:11:02 +0100

    comments added, right commit pushed

--- src/BooksSearch.js
+++ src/BooksSearch.js
@@ -1,3 +1,6 @@
+/*
+	* imports all the nesessary components 
+*/
 import React from 'react'
 import './App.css'
 import { Link } from 'react-router-dom' 
--- src/BooksShelf.js
+++ src/BooksShelf.js
@@ -1,3 +1,6 @@
+/*
+	* imports all the nesessary components 
+*/
 import React from 'react'
 import './App.css'
 import { Link } from 'react-router-dom' 
@@ -13,6 +16,7 @@ getShelfBooks(shelfName){
 componentDidMount(){
 	this.props.updateSearch();
 }	
+	
   render() {
     return (
      
